0f9d208457e0300236570a298e8ddf61285c4877
Ahmed Soltan <it.team-6@paytome.co>
2025-11-16 16:27:26 +0200

    middleware

--- app.js
+++ app.js
@@ -7,23 +7,49 @@ app.set("view engine","ejs");
 //enable send body 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+//middleware to serve static files  development mode 
+const morgan = require("morgan");
 
 app.listen(3000);
+
+
+
+
+
+//use middleware
+var middleware= require("./middleware/middleware");
+app.use(middleware);
+// diffrent use of middleware 
+app.use(morgan('dev')); //combined //tiny //dev logs 
+
+
+//Middleware for static files 
+app.use(express.static('public'));
+
+
+
+
+
 
 app.get("/", (req, res) => {
     // res.send("<p>Hello World, This is Ahmed Soltan</p>");
     res.sendFile('./views/index.html',{root:__dirname});
 });
-
 var routes=require("./routes/routes");
-
 app.use('',routes);
-
 //redirects
 app.get("/about-me", (req, res) => {
-    res.redirect("/about");
+    res.redirect("/about"); 
 });
+
+
+
+
+
+
+
+
+
 /**
  * Middleware
  */
